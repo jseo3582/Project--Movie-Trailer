3bf87a0092f3afc76ab2a077f52efb410e039629
James Seo <jseo3582@gmail.com>
2017-10-06 07:10:17 +0900

    Update media.py

--- media.py
+++ media.py
@@ -9,12 +9,12 @@ class Movie():
         movie_title,
         movive_storyline,
         poster_image,
-        trailer_youtube):
-
-    self.title = movie_title
-    self.storyline = movive_storyline
-    self.poster_image_url = poster_image
-    self.trailer_youtube_url = trailer_youtube
+        trailer_youtube
+    ):
+        self.title = movie_title
+        self.storyline = movive_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
 
     # open webbrowser when check the poster
     def show_trailer(self):
