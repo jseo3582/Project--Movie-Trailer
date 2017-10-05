8c9133d5de74acafe70ca3ea3b47ce22a13e9989
James Seo <jseo3582@gmail.com>
2017-10-06 02:56:31 +0900

    Update media.py

--- media.py
+++ media.py
@@ -1,11 +1,14 @@
 import webbrowser
 
 class Movie():
-	def __init__(self, movie_titile, movive_storyline, poster_image, trailer_youtube):
-		self.title = movie_titile
-		self.storyline = movive_storyline
-		self.poster_image_url = poster_image
-		self.trailer_youtube_url = trailer_youtube
-
-	def show_trailer(self):
-		webbrowser.open(self.trailer_youtube_url)
+    
+    # centents are defined here
+    def __init__(self, movie_titile, movive_storyline, poster_image, trailer_youtube):
+	self.title = movie_titile
+	self.storyline = movive_storyline
+	self.poster_image_url = poster_image
+	self.trailer_youtube_url = trailer_youtube
+	
+    # open webbrowser when check the poster
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
